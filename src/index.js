d7a769e3fba64c31629c25b2efef25e0120e0317
tokkikim <165551687+tokkikim@users.noreply.github.com>
2025-07-15 07:37:37 +0900

    Add root index export

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const { BaseAgent } = require('./agents/base-agent');
+const { WebTesterAgent } = require('./agents/web-tester');
+const { QualityInspectorAgent } = require('./agents/quality-inspector');
+
+module.exports = {
+  BaseAgent,
+  WebTesterAgent,
+  QualityInspectorAgent,
+};
